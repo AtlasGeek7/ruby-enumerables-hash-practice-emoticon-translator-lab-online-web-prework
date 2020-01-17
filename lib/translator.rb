fcd6e7203e879846e0f9a4012cb4ebd0a3d706ce
Khalid T <khalid.tissire@gmail.com>
2020-01-17 15:27:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(path)
   get_meaning = {}
   emo = YAML.load_file("./lib/emoticons.yml")
   emo.each_key do |key|
-    get_meaning.push(emo[key][1])
+    get_meaning[emo[key][1]] = 
   end
   return get_meaning
 end
