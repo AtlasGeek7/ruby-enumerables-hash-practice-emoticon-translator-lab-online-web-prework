845a876a69410dcdeecb3ca2ca6a245e1512a546
Khalid T <khalid.tissire@gmail.com>
2020-01-17 16:20:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,8 @@ def get_japanese_emoticon(path,emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path,emoticon)
+  hash = load_library(path)
+  return hash[:get_meaning][emoticon] if hash[:get_meaning][emoticon]
+  return "Sorry, that emoticon was not found"  
 end
